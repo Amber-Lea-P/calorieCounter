5d8010de70414e7a24d389accabebcec17f124a7
Amber Lea <112793091+Amber-Lea-P@users.noreply.github.com>
2024-03-24 17:04:57 +0200

    up to lesson 29

---
+++ script.js
@@ -0,0 +1,25 @@
+const calorieCounter = document.getElementById('calorie-counter');
+const budgetNumberInput = document.getElementById('budget');
+const entryDropdown = document.getElementById('entry-dropdown');
+const addEntryButton = document.getElementById('add-entry');
+const clearButton = document.getElementById('clear');
+const output = document.getElementById('output');
+
+let isError = false;
+
+//when we want to set the value wihin an input value, JS will receive a string value so we will have to write a funciton that converts the value of the string to a number. This is a helper function. 
+
+//I will keep this example here but this is the longer way of completing this function
+/* function cleanInputString(str) {
+    const strArray = str.split('');
+    const cleanStrArray = [];
+    for (let i = 0; i < strArray.length; i++) {
+        if (!["+", "-", " "].includes(strArray[i])) {cleanStrArray.push(strArray[i])};
+      }
+    } */
+
+//Regex example: Regular expressions are objects or tools that let us match patterns in strings in order to recognize them an manipulate them accordingly.
+function cleanInputString(str){
+    const regex = /[+-\s]/g;
+    return str.replace(regex, '');
+}
